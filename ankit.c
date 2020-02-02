893be3eaf403e968156e3d3643ca67a23f067be8
prajna131999 <60266922+prajna131999@users.noreply.github.com>
2020-02-02 13:04:52 +0530

    Add files via upload

---
+++ ankit.c
@@ -0,0 +1,68 @@
+
+
+
+#include<reg51.h>
+
+sbit AL=P1^0;
+sbit WT=P1^1;
+sbit CO=P1^2;
+sbit HD=P1^3;
+
+sbit R1=P1^0;
+sbit R2=P1^1;
+sbit R3=P1^2;
+sbit R4=P1^3;
+
+void main()
+{
+sbit AL==1;
+sbit WT==1;
+sbit CO==1;
+sbit HD==1;
+
+sbit R1=0;
+sbit R2=0;
+sbit R3=0;
+sbit R4=0;
+while(1)
+{
+    
+      if(AL==1)
+      {
+        R1==1
+}
+elseif
+{
+R1==0
+     
+}
+elseif(WT==1)
+{
+        R2==1
+}
+elseif
+{
+R2==0
+     
+}
+elseif(CO==1)
+{
+        R3==1
+}
+elseif
+{
+R3==0
+     elseif(HD==1)
+{
+        R4==1
+}
+else
+{
+R4==0
+     
+}
+}
+}
+}
+smart car.txt
+Displaying smart car.txt.
